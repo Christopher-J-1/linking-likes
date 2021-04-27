e757f4bd476ccca5f99dbcd1040d09ff06c2e7b5
Christopher Jones <chrisjtheonly@gmail.com>
2021-04-27 18:23:16 -0500

    LikingLikes

---
+++ script.js
@@ -0,0 +1,12 @@
+function likesinc1(){
+    var inc = document.querySelector("#likes1");
+    inc.innerHTML = parseInt(inc.textContent)+1;
+}
+function likesinc2(){
+    var inc = document.querySelector("#likes2");
+    inc.innerHTML = parseInt(inc.textContent)+1;
+}
+function likesinc3(){
+    var inc = document.querySelector("#likes3");
+    inc.innerHTML = parseInt(inc.textContent)+1;
+}
